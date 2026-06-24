5aec40a492417ddebdc867203ded0c165bd094b2
Elias2510 <eliasmilosi2002@gmail.com>
2026-06-24 17:18:19 +0300

    ad login and books

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import MyReadingLists from "./pages/MyReadingLists";
+import AdminDashboard from "./pages/AdminDashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+import "./App.css";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Navbar />
+
+            <main className="container">
+                <Routes>
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/" element={<Home />} />
+                    <Route path="/books" element={<Books />} />
+                    <Route path="/reading-lists" element={<MyReadingLists />} />
+
+                    <Route path="/admin" element={<AdminDashboard />} />
+                </Routes>
+            </main>
+        </BrowserRouter>
+    );
+}
+
+export default App;
